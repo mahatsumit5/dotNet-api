b804ef242cce723c114d18cab0649e612984938c
mahatsumit5 <mahatsumit5@gmail.com>
2024-09-28 16:04:07 +1000

    web api with .net framework and Entity Framework

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Models;
+
+namespace RealEstate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private static List <Category> categories=new List<Category>() 
+        {
+            new Category()
+            {             
+                Id = 0,                
+                ImageUrl="appartment.png",
+                Name="Appartment"
+            },
+            new Category()
+            {
+                Name="Commercial",
+                Id = 1,
+                ImageUrl="commercial.png"
+            }
+        };
+
+
+        [HttpGet]
+        public IEnumerable<Category> Get() {
+                        return categories;        
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody]Category category) 
+        {
+            categories.Add(category);
+        }
+        [HttpPut("{id}")]
+        public void Put(int id,[FromBody] Category category)
+        {
+            categories[id] = category;
+
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            categories.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstate.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string  ImageUrl { get; set; }
+
+    }
+}
